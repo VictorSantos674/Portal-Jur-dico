44ab656c1193c1e75163e241acb9495dcae8de09
Santos_Vic <142180844+VictorSantos674@users.noreply.github.com>
2025-02-07 19:25:59 +0000

    :card_file_box: docs: Atualização do App

--- src/App.js
+++ src/App.js
@@ -1,20 +1,31 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Navbar from "./components/Navbar";
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import { useAuth } from "./hooks/useAuth";
+import Login from "./pages/Login";
+import Dashboard from "./pages/Dashboard";
 import Home from "./pages/Home";
-import Processos from "./pages/Processos";
-import Contato from "./pages/Contato";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import "./styles/global.css";
+
+const ProtectedRoute = ({ element }) => {
+    const { isAuthenticated } = useAuth();
+    return isAuthenticated ? element : <Navigate to="/login" />;
+};
 
 function App() {
-  return (
-    <Router>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/processos" element={<Processos />} />
-        <Route path="/contato" element={<Contato />} />
-      </Routes>
-    </Router>
-  );
+    return (
+        <Router>
+            <Header />
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/dashboard" element={<ProtectedRoute element={<Dashboard />} />} />
+                <Route path="*" element={<Navigate to="/" />} />
+            </Routes>
+            <Footer />
+        </Router>
+    );
 }
 
 export default App;
