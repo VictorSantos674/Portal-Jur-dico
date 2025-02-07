49f7a63f4169b3a1074fa75593f86d999f4c6aa0
Santos_Vic <142180844+VictorSantos674@users.noreply.github.com>
2025-02-07 17:28:32 +0000

    :sparkles: Criando a Interface do Portal

---
+++ src/components/Navbar.js
@@ -0,0 +1,32 @@
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+
+const NavbarContainer = styled.nav`
+  background: #003366;
+  padding: 15px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const NavLinks = styled.div`
+  a {
+    color: white;
+    margin: 0 15px;
+    text-decoration: none;
+    font-size: 16px;
+  }
+`;
+
+export default function Navbar() {
+  return (
+    <NavbarContainer>
+      <h1 style={{ color: "white" }}>Tribunal Portal</h1>
+      <NavLinks>
+        <Link to="/">Home</Link>
+        <Link to="/processos">Processos</Link>
+        <Link to="/contato">Contato</Link>
+      </NavLinks>
+    </NavbarContainer>
+  );
+}
